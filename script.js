5b3046f0022be0b3673d138a4eba75acc9000667
Radhika Shetti <Radhika.shetti@nortonlifelock.com>
2023-03-13 22:28:37 -0700

    fix updateBackGroundColor method

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-let currentHr = dayjs().hour();
+let currentHr = dayjs().format('HH') // gets current hour
+currentHr=currentHr.toString()
+//let currentHr = 9;
 function updateBackGroundColor() {
   $('.time-block').each(function(){
       let scheduleHour = $(this).attr('id').split('-')[1]
-    console.log('scheduleHour',scheduleHour)
        if (currentHr > scheduleHour) {
          $(this).addClass('past')
        }
@@ -21,11 +22,11 @@ $('#hour-09 .description').val(localStorage.getItem('hour-09'))
 $('#hour-10 .description').val(localStorage.getItem('hour-10'))
 $('#hour-11 .description').val(localStorage.getItem('hour-11'))
 $('#hour-12 .description').val(localStorage.getItem('hour-12'))
-$('#hour-01 .description').val(localStorage.getItem('hour-01'))
-$('#hour-02 .description').val(localStorage.getItem('hour-02'))
-$('#hour-03 .description').val(localStorage.getItem('hour-03'))
-$('#hour-04 .description').val(localStorage.getItem('hour-04'))
-$('#hour-05 .description').val(localStorage.getItem('hour-05'))
+$('#hour-01 .description').val(localStorage.getItem('hour-1'))
+$('#hour-02 .description').val(localStorage.getItem('hour-2'))
+$('#hour-03 .description').val(localStorage.getItem('hour-3'))
+$('#hour-04 .description').val(localStorage.getItem('hour-4'))
+$('#hour-05 .description').val(localStorage.getItem('hour-5'))
 
 // display the current date in header
 $('#currentDay').text(dayjs().format('dddd MMMM D'));
