5567b8c187daec44dce1d716c731bbcedee29fe7
Radhika Shetti <Radhika.shetti@nortonlifelock.com>
2023-03-13 19:25:22 -0700

    remove the extra assets folder

--- Develop/script.js
+++ Develop/script.js
@@ -20,11 +20,11 @@ $('#hour-09 .description').val(localStorage.getItem('hour-09'))
 $('#hour-10 .description').val(localStorage.getItem('hour-10'))
 $('#hour-11 .description').val(localStorage.getItem('hour-11'))
 $('#hour-12 .description').val(localStorage.getItem('hour-12'))
-$('#hour-1 .description').val(localStorage.getItem('hour-1'))
-$('#hour-2 .description').val(localStorage.getItem('hour-2'))
+$('#hour-01 .description').val(localStorage.getItem('hour-01'))
+$('#hour-02 .description').val(localStorage.getItem('hour-02'))
 $('#hour-03 .description').val(localStorage.getItem('hour-03'))
-$('#hour-4 .description').val(localStorage.getItem('hour-4'))
-$('#hour-5 .description').val(localStorage.getItem('hour-5'))
+$('#hour-04 .description').val(localStorage.getItem('hour-04'))
+$('#hour-05 .description').val(localStorage.getItem('hour-05'))
 
 // display the current date in header
 $('#currentDay').text(dayjs().format('dddd MMMM D'));
